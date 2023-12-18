ec74a100599816b3b81a418984271647843cf8c7
Abdulrahman Rashwan <abdo.ra.1997@gmail.com>
2023-12-18 16:26:42 +0200

    replace anchor with button

--- src/components/Providers/ProvidersTable.js
+++ src/components/Providers/ProvidersTable.js
@@ -33,8 +33,7 @@ const ProvidersTable = (props) => {
     let pages;
     if (props.pagesCount > 0) {
         pages = (
-            <a
-                href="#"
+            <button
                 key="prev"
                 onClick={previousHandler}
                 className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white 
@@ -44,14 +43,13 @@ const ProvidersTable = (props) => {
                 }`}
             >
                 Previous
-            </a>
+            </button>
         );
 
         for (let i = 0; i < props.pagesCount; i++) {
             pages = [pages,
                 <li>
-                    <a
-                        href="#"
+                    <button
                         key={i}
                         onClick={pageChangeHandler.bind(null, i + 1)}
                         className={i + 1 === props.currentPage
@@ -59,15 +57,14 @@ const ProvidersTable = (props) => {
                             : "cursor-pointer flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
                     >
                         {i + 1}
-                    </a>
+                    </button>
                 </li>
             ];
         }
 
         pages = [...pages,
             <li>
-                <a
-                    href="#"
+                <button
                     key="next"
                     onClick={nextHandler}
                     className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
@@ -77,7 +74,7 @@ const ProvidersTable = (props) => {
                         }`}
                 >
                     Next
-                </a>
+                </button>
             </li>
         ];
     }
